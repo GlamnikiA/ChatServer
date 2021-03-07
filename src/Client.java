27f1077960fe863e0dbc18c9a01068ef6e1c16d2
Ardian Glamniki <ardian.glamniki@hotmail.com>
2021-03-07 15:49:58 +0100

    Initial commit

--- src/Client.java
+++ src/Client.java
@@ -163,8 +163,6 @@ public class Client {
         }
     }
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            new Client();
-        }
+        new Client();
     }
 }
